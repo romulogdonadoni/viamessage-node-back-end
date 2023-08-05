9b88f5e7c9156800313cf98c996b2326bc52d006
romulogdonadoni <romulogdonadoni@gmail.com>
2023-08-05 13:07:22 -0300

    3.2

--- src/routes/addFollow.js
+++ src/routes/addFollow.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const FollowModel = require('../models/followModel');
+const express = require("express");
+const FollowModel = require("../models/followModel");
 const router = express.Router();
-const { v4: uuidv4 } = require("uuid")
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
 
-router.post("/add/follow/:userid/:followtag", async (req, res) => {
-  const userid = req.params["userid"];
+router.post("/add/follow/:followtag", async (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+  const { id: userid } = jwt.decode(token);
   const followtag = req.params["followtag"];
 
   try {
@@ -12,11 +14,29 @@ router.post("/add/follow/:userid/:followtag", async (req, res) => {
       id: uuidv4(),
       followedUser: followtag,
       user_id: userid,
-    })
+    });
     res.send(JSON.stringify({ newFollow }));
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
 });
-module.exports = router
+router.get("/verify/follow/:followtag", async (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+  const { id: userid } = jwt.decode(token);
+  const followtag = req.params["followtag"];
+
+  try {
+    const newFollow = await FollowModel.findOne({ where: { user_id: userid, followedUser: followtag } });
+    if (newFollow) {
+      res.send(JSON.stringify({ liked: "following" }));
+    } else {
+      res.send(JSON.stringify({ liked: "follow" }));
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
