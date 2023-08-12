dc0e6a2eaef968dc591363154d0b0517763ff472
romulogdonadoni <romulogdonadoni@gmail.com>
2023-08-12 19:55:30 -0300

    4.2

--- src/routes/deletePost.js
+++ src/routes/deletePost.js
@@ -1,14 +1,17 @@
 const router = require("./authLogin");
 const authToken = require("../config/jwtConfig");
 const PostModel = require("../models/postModel");
+const jwt = require("jsonwebtoken");
 
 router.delete("/remove/post/:id", authToken, async (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+  const { user_id } = jwt.decode(token);
   const id = req.params["id"];
   try {
     await PostModel.destroy({
-      where: { id: id },
+      where: { id: id, user_id },
     });
-    res.status(201).json({msg: "Post deletado com sucesso!"});
+    res.status(201).json({ msg: "Post deletado com sucesso!" });
   } catch (error) {
     console.error("Erro ao criar post: " + error);
     res.sendStatus(500);
